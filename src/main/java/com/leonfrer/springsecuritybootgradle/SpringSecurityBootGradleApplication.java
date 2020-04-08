f1dcc586bf7508bbb688f7118b5577607e0fc46c
leon <leonfrer@gmail.com>
2020-04-09 00:11:40 +0800

    spring security + spring boot + gradle build

---
+++ src/main/java/com/leonfrer/springsecuritybootgradle/SpringSecurityBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.leonfrer.springsecuritybootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBootGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonfrer/springsecuritybootgradle/SpringSecurityBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonfrer.springsecuritybootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
